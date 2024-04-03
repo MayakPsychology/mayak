2ca6a24d1f2ff95056d623f4e606eeade3c8e355
watchlar2000 <sergii.koch@gmail.com>
2024-04-03 19:49:00 +0300

    [MID-113] remove motion.div

--- src/app/_components/MainPageSections/DistrictList.js
+++ src/app/_components/MainPageSections/DistrictList.js
@@ -7,7 +7,6 @@ import { CheckMark, Search } from '@icons';
 import { PillButton } from '@components/PillButton';
 import { Slide, Slider } from '@components/Slider';
 import { cn } from '@utils/cn';
-import { motion } from 'framer-motion';
 
 const activeButtonStyles = 'pointer-events-none border-secondary-300 bg-secondary-300 font-semibold text-gray-900';
 const commonIconStyle = 'h-4 w-4 transition-all';
@@ -16,18 +15,6 @@ export function DistrictList({ list, className }) {
   const [selected, setSelected] = useState(0);
   const handleClick = index => {
     setSelected(index);
-  };
-
-  const motionOnHover = {
-    rest: { x: 0, ease: 'easeOut', duration: 0.4, type: 'spring' },
-    hover: {
-      x: -7.5,
-      transition: {
-        duration: 0.4,
-        type: 'spring',
-        ease: 'easeIn',
-      },
-    },
   };
 
   return (
@@ -42,28 +29,31 @@ export function DistrictList({ list, className }) {
               className={cn(isSelected && 'pointer-events-none cursor-none')}
               tabIndex={-1}
             >
-              <motion.div variants={motionOnHover} animate="rest" whileHover="hover" whileFocus="hover">
-                <PillButton
-                  variant="eventFilter"
-                  colorVariant="semiorange"
-                  icon={[
-                    isSelected && (
-                      <CheckMark
-                        key={`checkmark+${index}`}
-                        className={cn('block group-hover:hidden group-focus:hidden', commonIconStyle)}
-                      />
-                    ),
-                    <Search
-                      key={`searchicon+${index}`}
-                      className={cn('hidden group-hover:block group-focus:block', commonIconStyle)}
-                    />,
-                  ]}
-                  className={cn(isSelected && activeButtonStyles, 'group', { 'w-[170px]': index !== 0 })}
-                  aria-label={`Click to see specialists related to the district with id ${id}`}
-                >
-                  {name}
-                </PillButton>
-              </motion.div>
+              <PillButton
+                variant="eventFilter"
+                colorVariant="semiorange"
+                icon={[
+                  isSelected && (
+                    <CheckMark
+                      key={`checkmark+${index}`}
+                      className={cn('block group-hover:hidden group-focus:hidden', commonIconStyle)}
+                    />
+                  ),
+                  <Search
+                    key={`searchicon+${index}`}
+                    className={cn('hidden group-hover:block group-focus:block', commonIconStyle)}
+                  />,
+                ]}
+                className={cn(
+                  isSelected && activeButtonStyles,
+                  'group',
+                  { 'w-[170px]': index !== 0 },
+                  { 'w-[100px]': index === 0 },
+                )}
+                aria-label={`Click to see specialists related to the district with id ${id}`}
+              >
+                {name}
+              </PillButton>
             </Link>
           </Slide>
         );
