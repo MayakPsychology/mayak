f7ace12170967afcd89ef9f55d611fd0fb643246
BigTako <libert761@gmail.com>
2024-04-05 20:01:04 +0300

    [MID-129] code refactoring

--- src/app/(admin)/admin/_lib/validationSchemas/donationDetailsSchema.js
+++ src/app/(admin)/admin/_lib/validationSchemas/donationDetailsSchema.js
@@ -1,23 +1,15 @@
 import { z } from 'zod';
 import iban from 'iban';
-import { MESSAGES, zBoolean, zString, zUrl } from './common';
+import { zBoolean, zString, zUrl } from './common';
 
 export const DonationDetailsSchema = z.object({
   donationEnabled: zBoolean,
-  title: z
-    .string({
-      required_error: MESSAGES.requiredField,
-      invalid_type_error: MESSAGES.requiredField,
-    })
-    .trim(),
-  subtitle: z
-    .string({
-      required_error: MESSAGES.requiredField,
-      invalid_type_error: MESSAGES.requiredField,
-    })
-    .max(50, {
-      message: 'Довжина підзаголовку не повинна перевищувати 50-ти символів',
-    }),
+  title: zString.max(128, {
+    message: 'Довжина заголовку не повинна перевищувати 128 символів',
+  }),
+  subtitle: zString.max(50, {
+    message: 'Довжина підзаголовку не повинна перевищувати 50-ти символів',
+  }),
   subtitleEnabled: zBoolean,
   paypalLink: zUrl,
   paypalLinkEnabled: zBoolean,
@@ -26,7 +18,7 @@ export const DonationDetailsSchema = z.object({
   bankDetailsEnabled: zBoolean,
   enterpriceName: zString,
   iban: zString.refine(value => iban.isValid(value), {
-    message: 'Invalid IBAN',
+    message: 'Введіть коректний IBAN',
   }),
   enterpriseRegisterId: z.string().refine(value => value.length === 8 && !Number.isNaN(Number(value)), {
     message: 'Код ЄДРПОУ має містити тільки цифри та мати довжину 8 символів',
