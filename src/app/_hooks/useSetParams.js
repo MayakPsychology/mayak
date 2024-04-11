e375bd2c627c346ce3b36cc71d09ad6c9a6a34cb
Olexander Popov <alex.popov.tech@gmail.com>
2024-04-11 13:17:22 +0300

    [MID-179] review fixes

--- src/app/_hooks/useSetParams.js
+++ src/app/_hooks/useSetParams.js
@@ -4,28 +4,24 @@ export function useSetParam(param) {
   const searchParams = useSearchParams();
   const router = useRouter();
   const newParams = new URLSearchParams(searchParams);
+  const replaceWithNewParams = () => router.replace(`?${newParams.toString()}`);
+
   const add = value => {
     newParams.append(param, value);
-    router.replace(`?${newParams.toString()}`);
+    replaceWithNewParams();
   };
 
   const replace = value => {
     newParams.delete(param);
-    if (Array.isArray(value)) {
-      value.forEach(v => newParams.append(param, v));
-    } else {
-      newParams.set(param, value);
-    }
-    router.replace(`?${newParams.toString()}`);
+    const values = Array.isArray(value) ? value : [value];
+    values.forEach(v => newParams.append(param, v));
+    replaceWithNewParams();
   };
 
   const remove = value => {
-    if (value) {
-      newParams.delete(param, value);
-    } else {
-      newParams.delete(param);
-    }
-    router.replace(`?${newParams.toString()}`);
+    newParams.delete(param, value);
+    replaceWithNewParams();
   };
+
   return { add, replace, set: replace, remove };
 }
