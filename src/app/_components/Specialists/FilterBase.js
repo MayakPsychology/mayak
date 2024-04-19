7e8359e39beadcca0984b75c48a7ce7590927d85
Ivan-Koshan <ivan.koshan@keenethics.com>
2024-04-19 23:31:39 +0300

    [MID-197] fix filter display cutoff

--- src/app/_components/Specialists/FilterBase.js
+++ src/app/_components/Specialists/FilterBase.js
@@ -4,10 +4,17 @@ import { useEffect, useRef, useState } from 'react';
 import { FilterChip } from '@components/FilterChip';
 import PropTypes from 'prop-types';
 import { cn } from '@/utils/cn';
+import { useMediaQuery } from '@/app/_hooks';
+import { screens } from '@/app/styles/tailwind/ui';
 
-export function FilterBase({ count, children, filterText }) {
+export function FilterBase({ count, children, filterText, originFromRight = false }) {
   const [opened, setOpened] = useState(false);
+  const [filterWidth, setFilterWidth] = useState(0);
   const dropdownRef = useRef(null);
+  const filterRef = useRef(null);
+
+  const isScreenMd = useMediaQuery(`(min-width: ${screens.md})`);
+  const dropdownWidth = isScreenMd ? 320 : 300;
 
   const handleClickOutside = event => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target) && opened) {
@@ -22,9 +29,24 @@ export function FilterBase({ count, children, filterText }) {
     };
   });
 
+  useEffect(() => {
+    const node = filterRef.current;
+    if (!node) return undefined;
+
+    const observer = new ResizeObserver(elements => {
+      setFilterWidth(elements[0].target.getBoundingClientRect().width);
+    });
+    observer.observe(node);
+
+    return () => {
+      observer.unobserve(node);
+    };
+  }, [filterRef.current]);
+
   return (
     <div className="relative">
       <FilterChip
+        ref={filterRef}
         opened={opened}
         text={filterText}
         count={count}
@@ -34,9 +56,16 @@ export function FilterBase({ count, children, filterText }) {
       />
       <div
         ref={dropdownRef}
-        className={cn(`z-10 -mr-[255px] hidden w-max flex-col rounded-3xl border bg-other-white px-2 py-1`, {
-          flex: opened,
-        })}
+        className={cn(
+          `z-15 relative -mr-[255px] box-border hidden  flex-col rounded-3xl border bg-other-white px-2 py-1`,
+          'w-max max-w-[300px] md:max-w-[320px]',
+          {
+            flex: opened,
+          },
+        )}
+        style={{
+          left: originFromRight ? `-${dropdownWidth - filterWidth}px` : '',
+        }}
       >
         {children}
       </div>
@@ -48,4 +77,5 @@ FilterBase.propTypes = {
   count: PropTypes.number,
   children: PropTypes.node,
   filterText: PropTypes.string,
+  originFromRight: PropTypes.bool,
 };
