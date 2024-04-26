89a7c3fbc67beb1eeb5e475b6b7083d02ccf34af
Maksym Zakharov <maxy121997@gmail.com>
2024-04-26 14:03:39 +0200

    [MID-206] update faq

--- src/app/_components/MainPageSections/FAQCard.js
+++ src/app/_components/MainPageSections/FAQCard.js
@@ -3,11 +3,11 @@ import Plus from '@icons/plus.svg';
 import Minus from '@icons/minus.svg';
 import { FAQCardPropTypes } from './prop-types';
 
-export function FAQCard({ id, question, answer }) {
+export function FAQCard({ id, question, answer, checked, onChange }) {
   return (
     <div className="flex w-full max-w-[906px] flex-col gap-2 rounded-[40px] border-[1px] border-gray-600 bg-other-white px-6 py-5">
       <div className="group peer relative flex justify-between">
-        <input type="checkbox" className="hidden" id={id} />
+        <input type="checkbox" className="hidden" id={id} checked={checked} onChange={onChange} />
         <label htmlFor={id} className="cursor-pointer">
           <Paragraph className="text-p1 font-bold text-primary-600">{question}</Paragraph>
         </label>
@@ -18,7 +18,7 @@ export function FAQCard({ id, question, answer }) {
           </label>
         </div>
       </div>
-      <Paragraph className="hidden text-p2 text-gray-900 peer-has-[input:checked]:inline">{answer}</Paragraph>
+      <Paragraph className="hidden text-p2 text-gray-900 peer-has-[input:checked]:inline whitespace-pre-line">{answer}</Paragraph>
     </div>
   );
 }
--- src/app/_components/MainPageSections/FAQSection.js
+++ src/app/_components/MainPageSections/FAQSection.js
@@ -1,10 +1,17 @@
+'use client';
+
 import PropTypes from 'prop-types';
 import { Heading } from '@components/Typography';
 import { TopWave } from '@components/TopWave';
 import { FAQCard } from '@components/MainPageSections/FAQCard';
+import { useState } from 'react';
 import { FAQCardPropTypes } from './prop-types';
 
 export function FAQSection({ faqs }) {
+  const [checked, setChecked] = useState(null);
+  const onCheck = id => {
+    setChecked(checked === id ? null : id);
+  }
   return (
     <section className="relative flex flex-col gap-4 bg-secondary-100 pb-[46px] lg:gap-12 lg:pb-[80px]">
       <TopWave className="h-[19px] bg-other-white lg:h-12" />
@@ -15,7 +22,7 @@ export function FAQSection({ faqs }) {
       </div>
       <div className="flex w-full flex-col items-center gap-4 px-4">
         {faqs.map(faq => (
-          <FAQCard key={faq.id} {...faq} />
+          <FAQCard key={faq.id} {...faq} checked={checked === faq.id} onChange={() => onCheck(faq.id)} />
         ))}
       </div>
     </section>
