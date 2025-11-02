71ebf7b33cdd4a4c45e2685f84a923271f580fb1
vitalii-mieliet <vitalii.mieliet@gmail.com>
2025-11-02 14:07:22 +0000

    [TASK-14] feat(routes): add ROUTES constant for navigation links

---
+++ src/app/config/routes.js
@@ -0,0 +1,9 @@
+const ROUTES = {
+  APPLY: '/apply',
+  APPLY_SPECIALIST: '/apply/specialist',
+  APPLY_ORGANIZATION: '/apply/organization',
+  APPLY_EVENT: '/apply/event',
+  SPECIALIST: '/specialist',
+  EVENTS: '/event',
+};
+export default ROUTES;
