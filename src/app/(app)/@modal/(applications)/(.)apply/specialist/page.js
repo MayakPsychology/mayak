dda1c862b72190ac101550d0aaf14e154f9e4c2d
vitalii-mieliet <vitalii.mieliet@gmail.com>
2025-11-14 15:32:01 +0000

    [TASK-14]  feat(applications): add SpecialistApplicationWizard component and modal page for specialist applications

---
+++ src/app/(app)/@modal/(applications)/(.)apply/specialist/page.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { SpecialistApplicationWizard } from '@/app/_components/applications/SpecialistApplicationWizard';
+
+export default function AddNewSpecialistModalPage() {
+  return <SpecialistApplicationWizard />;
+}
