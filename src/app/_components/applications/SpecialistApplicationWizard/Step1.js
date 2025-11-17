0b4dc69efca01f2fe999f7ba78d63e3423f793f2
vitalii-mieliet <vitalii.mieliet@gmail.com>
2025-11-17 20:21:35 +0000

    [TASK-14] feat(applications): add DistrictsGroup component and integrate into Step1

--- src/app/_components/applications/SpecialistApplicationWizard/Step1.js
+++ src/app/_components/applications/SpecialistApplicationWizard/Step1.js
@@ -1,12 +1,14 @@
 'use client';
 
 import React from 'react';
+import { DistrictsGroup } from '../_shared/field-groups';
 import { SpecialistGeneralInfo } from './field-groups';
 
 export function Step1() {
   return (
     <div className="flex w-full flex-col gap-14 sm:gap-11 lg:w-full lg:max-w-none lg:gap-10">
       <SpecialistGeneralInfo />
+      <DistrictsGroup />
     </div>
   );
 }
