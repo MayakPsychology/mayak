90cf558d5327d4199178907649853cbe94e3ad3e
vitalii-mieliet <vitalii.mieliet@gmail.com>
2025-11-18 17:21:12 +0000

    [TASK-14] feat(applications): add DescriptionField component and integrate into Step1

---
+++ src/app/_components/applications/_shared/fields/DescriptionField.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+import { TextArea } from '@/app/_components/TextArea';
+
+export function DescriptionField() {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+  return (
+    <div>
+      <label className="text-base mb-2 block font-medium">
+        Що ми можемо добавити добавити про Вас у пункт Опис? <span className="text-red-500">*</span>
+      </label>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <TextArea {...field} maxLength={320} placeholder="Ваша відповідь" required error={errors?.message?.message} />
+        )}
+      />
+    </div>
+  );
+}
