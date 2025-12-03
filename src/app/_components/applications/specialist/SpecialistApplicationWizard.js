bd1be197490951940fa775ef95e052bdf1822524
vitalii-mieliet <vitalii.mieliet@gmail.com>
2025-12-03 10:58:26 +0000

    [TASK-14] feat(specialist): Create Step 4 component for therapy types and pricing

--- src/app/_components/applications/specialist/SpecialistApplicationWizard.js
+++ src/app/_components/applications/specialist/SpecialistApplicationWizard.js
@@ -8,27 +8,33 @@ import { PillButton } from '../../PillButton';
 import { Step1 } from './Step1';
 import { Step2 } from './Step2';
 import { Step3 } from './Step3';
+import { Step4 } from './Step4';
 
 export function SpecialistApplicationWizard({ dicts }) {
-  const { clientCategories, specializations, specializationMethods, districts } = dicts;
+  const { clientCategories, specializations, specializationMethods, districts, therapies, requests } = dicts;
+
   const [step, setStep] = useState(1);
   const methods = useForm({
     defaultValues: specialistDefaultValues,
     mode: 'onChange',
   });
-  // const onSubmit = data => console.log(data);
+  const onSubmit = data => {
+    // eslint-disable-next-line no-console
+    console.clear();
+    // eslint-disable-next-line no-console
+    console.log(data);
+  };
 
   return (
     <div>
       <p>Specialist Wizard Component</p>
 
       <FormProvider {...methods}>
-        <form
-        // onSubmit={methods.handleSubmit(onSubmit)}
-        >
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
           {step === 1 && <Step1 districts={districts} />}
           {step === 2 && <Step2 clientCategories={clientCategories} />}
           {step === 3 && <Step3 specializations={specializations} specializationMethods={specializationMethods} />}
+          {step === 4 && <Step4 therapies={therapies} requests={requests} />}
 
           {/* Step4 */}
           <div className="flex justify-between">
@@ -49,10 +55,7 @@ export function SpecialistApplicationWizard({ dicts }) {
               Далі
             </PillButton>
           </div>
-          <PillButton
-            type="submit"
-            // onClick={methods.handleSubmit(onSubmit)}
-          >
+          <PillButton type="submit" onClick={methods.handleSubmit(onSubmit)}>
             Submit
           </PillButton>
         </form>
@@ -67,5 +70,7 @@ SpecialistApplicationWizard.propTypes = {
     clientCategories: PropTypes.array.isRequired,
     specializations: PropTypes.array.isRequired,
     specializationMethods: PropTypes.array.isRequired,
+    therapies: PropTypes.array.isRequired,
+    requests: PropTypes.array.isRequired,
   }).isRequired,
 };
