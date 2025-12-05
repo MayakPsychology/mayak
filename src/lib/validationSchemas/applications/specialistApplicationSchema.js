b1156b8c5d56375112d6639fd266f716f68cee7b
vitalii-mieliet <vitalii.mieliet@gmail.com>
2025-12-05 13:33:48 +0000

    [TASK-14] feat(validation): add validation for time intervals and row-level error display

--- src/lib/validationSchemas/applications/specialistApplicationSchema.js
+++ src/lib/validationSchemas/applications/specialistApplicationSchema.js
@@ -1,13 +1,36 @@
 import { z } from 'zod';
 import { FormatOfWork, Gender } from '@prisma/client';
+import { WEEKDAYS_TRANSLATION } from '@/app/(admin)/admin/_lib/consts';
 import { string, number, boolean, array } from '@/lib/validationSchemas/utils';
 
-const zCreateAddressSchema = z.object({
+export const zCreateAddressSchema = z.object({
   fullAddress: string('Адреса').min(2).max(128).zod,
   district: string('Район').min(2).max(128).zod,
   nameOfClinic: string('Назва клініки').min(2).max(128).emptyToNull().zod,
   isPrimary: boolean('Основна').zod,
 });
+
+export const zWorkDaySchema = z
+  .object({
+    weekDay: z.enum(Object.values(WEEKDAYS_TRANSLATION)),
+    time: z
+      .string()
+      .refine(val => !val || /\d{2}:\d{2}\s-\s\d{2}:\d{2}/.test(val), {
+        message: 'Введіть час у форматі ХХ:ХХ - ХХ:ХХ',
+      })
+      .nullish(),
+    isDayOff: z.boolean().nullish(),
+  })
+  .superRefine((data, ctx) => {
+    const { time, isDayOff } = data;
+    if (time && isDayOff) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Приберіть час роботи, якщо це вихідний',
+        path: ['time'],
+      });
+    }
+  });
 
 export const specialistApplicationSchema = z.object({
   firstName: string("Ім'я").min(2).max(128).zod,
@@ -23,4 +46,5 @@ export const specialistApplicationSchema = z.object({
     invalid_type_error: 'Оберіть формат роботи',
   }),
   addresses: array('Адреси', zCreateAddressSchema).zod,
+  workTime: array('Адреси', zWorkDaySchema).zod,
 });
